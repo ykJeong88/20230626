1285a015071fa872bbd96e2bbff462e440f2307a
ykjeong <mohargga@gmail.com>
2023-06-26 17:08:04 +0900

    setting

---
+++ src/test/java/project/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
