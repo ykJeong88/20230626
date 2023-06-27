c04165736da8aa3c4ec2f68404b37a7a8a3ba312
ykjeong <mohargga@gmail.com>
2023-06-27 22:14:05 +0900

    독립 실행형 서블릿 애플리케이션

--- src/main/java/project/myproject/HelloController.java
+++ src/main/java/project/myproject/HelloController.java
@@ -3,10 +3,9 @@ package project.myproject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class HelloController {
 
-    @GetMapping("/hello")
     public String hello(String name){
         // branch test
         return "Hello " + name;
