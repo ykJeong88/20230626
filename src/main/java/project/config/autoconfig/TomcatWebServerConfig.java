31ed69ee4023adc0b30c4734d0534bd434702b68
ykjeong <mohargga@gmail.com>
2023-07-10 22:22:43 +0900

    [외부 설정을 이용한 자동 구성] ㄴ Environment 추상화와 프로퍼티 ㄴ 자동 구성에 Environment 프로퍼티 적용 ㄴ @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/project/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/project/config/autoconfig/TomcatWebServerConfig.java
@@ -1,10 +1,12 @@
 package project.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.env.Environment;
 import project.config.ConditionalMyOnClass;
 import project.config.MyAutoConfiguration;
 
@@ -12,10 +14,16 @@ import project.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(this.contextPath);
+
+        return factory;
     }
 
 }
